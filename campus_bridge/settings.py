91bba442232e419d1609b1f47d7d9ab2f3d44ef7
Dinesh4953 <dinu08642@gmail.com>
2025-10-03 23:12:15 +0530

    wsgi config-1

--- campus_bridge/settings.py
+++ campus_bridge/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
--- campus_bridge/wsgi.py
+++ campus_bridge/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campus_bridge.settings")
 
 application = get_wsgi_application()
+app = application
