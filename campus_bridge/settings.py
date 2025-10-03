acd150c3df37b03e957c931f4d3788ccda24acb7
Dinesh4953 <dinu08642@gmail.com>
2025-10-03 23:19:46 +0530

    wsgi config-2

--- campus_bridge/settings.py
+++ campus_bridge/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "dev-secret")
+SECRET_KEY = "django-insecure-1zd-()-(7g%ujzdsq+xrh5#p@$ahp**u)w3i81q1h_6&!xrmi3"
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
